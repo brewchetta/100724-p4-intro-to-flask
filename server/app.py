4a73910fe83eb7b2dc8a3ee0e240617c066d01b5
brewchetta <chett.tiller@gmail.com>
2024-12-09 12:12:39 -0500

    what is your favorite tv show?

--- server/app.py
+++ server/app.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python3
 
-from flask import make_response, jsonify
+from flask import make_response, jsonify, request
 from config import app
 
 @app.get('/')
 def index():
-    return make_response( "<h1>Hello World</h1>" )
+    return make_response( "<h1>Hello World</h1>", 404 )
+
+
+@app.get('/welcome')
+def welcome():
+    return make_response( "<h2>Welcome!</h2><p>This is my website...</p>", 200 )
+
+
+@app.get('/tv-shows')
+def all_shows():
+    tv_shows = [
+        { "name": "Warehouse Eureka" },
+        { "name": "One Piece" },
+        { "name": "Teletubbies" }
+    ]
+
+    return make_response( jsonify( tv_shows ), 200 )
+
+
+@app.post('/tv-shows')
+def post_tv_shows():
+    new_show = request.json
+
+    return f"<p>Your new show is {new_show['name']} with a rating of {new_show['rating']}</p>"
+
+
+# with a parameter
+@app.get('/tv-shows/<int:number_of_views>/<int:rating>')
+def show_by_id(number_of_views, rating):
+
+    return f"You're looking for a show with number of views {number_of_views} with a rating of {rating}"
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
